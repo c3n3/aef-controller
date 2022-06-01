d7a1b929732d5b28ea8928c5193e34f4028cc506
Caden Churchman <ccaden21@gmail.com>
2022-05-31 22:09:09 -0500

    Start of the app

---
+++ aef-controller/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main">
+      <ChangeableList type="button" :inputs="inputs"/>
+      <ChangeableList type="encoder" :inputs="inputs"/>
+  </div>
+</template>
+
+<script>
+import ChangeableList from './components/ChangableList.vue'
+import inputjson from './input.json'
+
+export default {
+  name: 'App',
+  components: {
+    ChangeableList
+  },
+  data() {
+    return {
+      inputs: inputjson
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.main {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ aef-controller/src/components/ChangableList.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="list-container">
+    <h3>{{type}}s</h3>
+    <div v-for="input, index in inputs" :key="index" class="list">
+      <div v-if="input['type'] == type">
+        {{input['name']}}
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'ChangeableList',
+  props: {
+    type: String,
+    inputs: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.list-container {
+  width: 50%;
+  height: 400px;
+  overflow:hidden; overflow-y:scroll;
+}
+
+
+/* nav ul {
+  height: 200px;
+  width: 18%;
+} */
+.list {
+}
+</style>
